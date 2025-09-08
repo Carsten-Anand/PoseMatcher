e8e2eab46e332398721eb891e8adcbc766333503
Emerald2012 <phyoemeraldaung@gmail.com>
2025-09-08 15:06:09 +0800

    Update ContentView.swift

--- PoseMatcher/ContentView.swift
+++ PoseMatcher/ContentView.swift
@@ -1,57 +1,25 @@
-//import SwiftUI
-//
-//
-//
-//struct ContentView: View {
-//    @State private var images = [CGImage]()
-//    @State private var selectedImagesFromPicker: [Image] = []
-//
-//    
-//    
-//    var body: some View {
-//        HStack{
-//           LiveViews()
-//            StaticViews(image: Image(<#String#>) as! CGImage)
-//            PhotoPickerView(selectedImages: $selectedImagesFromPicker)
-//
-//            //PhotoPickerView(selectedImages: $images, selectedCGImages: <#[CGImage]#>)
-//            StaticViews(image:images as! CGImage)
-//        }
-//    }
-//}
-//
-//#Preview {
-//    ContentView()
-//}
-
 import SwiftUI
-import PhotosUI
+
+
 
 struct ContentView: View {
-    @State private var images: [Image] = []
+    @State private var images = [CGImage]()
+    @State private var selectedImagesFromPicker: [Image] = []
+
     
     
     var body: some View {
-        HStack {
-            
-            if images.first != nil {
-                
-                if let cgImagePlaceholder = UIImage(systemName: "photo")?.cgImage {
-                     StaticViews(image: cgImagePlaceholder)
-                }
-            } else {
-                 
-                 Text("No static image loaded")
-            }
+        HStack{
+           LiveViews()
+            StaticViews(image: Image(<#String#>) as! CGImage)
+            PhotoPickerView(selectedImages: $selectedImagesFromPicker)
 
-            
-            PhotoPickerView(selectedImages: $images)
-            
-            
+            //PhotoPickerView(selectedImages: $images, selectedCGImages: <#[CGImage]#>)
+            StaticViews(image:images as! CGImage)
         }
     }
 }
 
 #Preview {
-    ContentView()
+   ContentView()
 }
